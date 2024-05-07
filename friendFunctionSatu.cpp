2d7cda4bc2a81026853c4e756575e04621beb5dd
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-07 16:00:14 +0700

    membuat class manusia

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 class mahasiswa
 {
-    
+private:
+      string nama;
+
+public:
+     friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama =b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama (joko, "kairi kumar");
+    return 0;
 }
