ee81f6ddb8177af1e3eb200bed482012097203b0
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-07 15:21:34 +0700

    membuat object mhs

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -11,5 +11,16 @@ class mahasiswa{
 };
 
 int main (){
-    
+
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer deference pMhs
+    pMhs-> nim = 3 //Arrow Operator 
+    pMhs ->showNim();
+    return 0;
 }
